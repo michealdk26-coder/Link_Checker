a66599965a04113aafe4bea5d5ed07cbe3a2394b
unknown <michealdk26@gmail.com>
2026-01-10 21:46:13 -0800

    Remove serving of signup/login/dashboard pages; index-only scanning

--- server.js
+++ server.js
@@ -63,17 +63,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
-});
+// Removed legacy pages: signup, login, dashboard
 
 // Error handling middleware
 app.use((err, req, res, next) => {
